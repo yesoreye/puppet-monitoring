489ac911a04a873151638fad2b7ca2b37882cf50
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2018-10-19 15:40:13 +0100

    .

--- Puppetfile
+++ Puppetfile
@@ -3,3 +3,4 @@ forge "https://forge.puppet.com"
 mod 'puppetlabs-stdlib', '4.25.1'
 mod 'dwerder-graphite', '7.3.0'
 mod 'puppet-nginx', '0.14.0'
+mod 'puppet-selinux', '1.6.1'
--- site.pp
+++ site.pp
@@ -1,9 +1,53 @@
 # region Class: monitoring::grafana_stack::nginx::allinone
 class monitoring::grafana_stack::nginx::allinone {
   notice('Nginx All in one class')
+  selinux::port {
+    'allow-graphite-8888' :
+      ensure   => 'present',
+      seltype  => 'http_port_t',
+      protocol => 'tcp',
+      port     => 8888,
+  }
   ensure_resource('file', [ '/etc/nginx/conf.d' ],{
     ensure => directory,
   })
+  ::nginx::resource::server { 'graphitewebserver' :
+    ensure               => present,
+    server_name          => ['_'],
+    listen_port          => 8888,
+    client_max_body_size => '64M',
+    use_default_location => false,
+    locations            => {
+      '^~ /static/'  =>  {
+        location_cfg_append => {
+          root       => '/usr/share/pyshared/django/contrib/admin',
+          access_log => 'off',
+        }
+      },
+      '^~ /content/' =>  {
+        location_cfg_append => {
+          root       => '/opt/graphite/webapp/',
+          access_log => 'off',
+        },
+        expires             => '30d',
+      },
+      '/'            => {
+        proxy_set_header    => ['Host $http_host','X-Real-IP $remote_addr'],
+        location_cfg_append => {
+          proxy_pass_header     => 'Server',
+          proxy_redirect        =>'off',
+          proxy_connect_timeout => 10,
+          proxy_read_timeout    => 60,
+          proxy_pass            => 'http://unix:/var/run/graphite.sock',
+        }
+      }
+    },
+    notify               => Service['nginx'],
+    require              => [
+      Service['nginx'],
+      Selinux::Port['allow-graphite-8888']
+    ],
+  }
 }
 #endregion
 
