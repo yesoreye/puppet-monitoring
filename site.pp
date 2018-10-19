07a8546211a0a021a6b5072b0a850fe8eb38f2eb
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2018-10-19 13:35:48 +0100

    .

--- site.pp
+++ site.pp
@@ -1,16 +1,38 @@
-# Class: monitoring::base
-#
-#
+# region Class: monitoring::grafana_stack::graphite::base
+class monitoring::grafana_stack::graphite::base {
+  notice('Graphite base class')
+}
+#endregion
+
+# region Class: monitoring::grafana_stack::grafana::base
+class monitoring::grafana_stack::grafana::base {
+  notice('Grafana base class')
+}
+#endregion
+
+# region Class: monitoring::grafana_stack::base
+class monitoring::grafana_stack::base {
+  notice('Grafana Stack base class')
+}
+#endregion
+
+# region Class: monitoring::base
 class monitoring::base {
 
   notice('Base class')
 
 }
+#endregion
 
+# region Node: Node settings
 node default {
 
   notice(lookup('welcome'))
 
   include ::monitoring::base
+  include ::monitoring::grafana_stack::base
+  include ::monitoring::grafana_stack::graphite::base
+  include ::monitoring::grafana_stack::grafana::base
 
 }
+#endregion
