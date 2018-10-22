1fed6d68a1d2946394ac7c323a5ef28f0b1f8436
Sriramajeyam <yesoreyeram@gmail.com>
2018-10-22 17:27:22 +0100

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,12 @@
 
 echo "Testing Graphite";
 
-echo "foo.bar 321 `date +%s`" | nc localhost 2003 && echo "Metric foo.bar with value 321 sent to graphite on `date`"
+echo "foo.bar 321 `date +%s`" | nc localhost 2003 && echo "Metric foo.bar with value 321 sent to graphite port 2003 on `date`"
 
 curl "http://localhost:8888/render?format=json&target=foo.bar&from=-3minutes"
+
+echo "foo.baz 333 `date +%s`" | nc localhost 2018 && echo "Metric foo.baz with value 333 sent to graphite port 2018 on `date`"
+
+curl "http://localhost:8888/render?format=json&target=foo.baz&from=-3minutes"
 
 exit 0;
